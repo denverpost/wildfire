6a348b914d192b101cbd495b0f06d6ce2e0cd364
freejoe76 <jmurphy@denverpost.com>
2015-04-14 18:52:56 -0400

    Start on the regex

--- diffs/geomacparser.py
+++ diffs/geomacparser.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # Parse GEOMAC's list of KML files and return HTML or a CSV.
 import os, sys, doctest, csv, argparse
+import re
+from datetime import datetime
+#from bs4 import BeautifulSoup
 
 # Takes a filename as input and goes line-by-line through the file.
 # Usage:
@@ -12,7 +15,24 @@ import os, sys, doctest, csv, argparse
 #    echo "Couldn't find file $FILE, please pass a --file FILENAME argument to this script."
 
 def main(args):
-    pass
+    fn = 'kml_list.%s.current' % args.state
+    fh = open(fn, 'rb')
+    content = []
+    url_prefix = 'http://rmgsc.cr.usgs.gov/outgoing/GeoMAC/current_year_fire_data/KMLS/'
+
+    for line in fh.readlines():
+        if 'Last modified' in line:
+            continue
+        line = line.strip()
+        #print line
+        pattern = '.*href="(?P<href>[^"]+)">(?P<slug>[^ ]+)\ (?P<fire>[^0-9]+)(?P<date>[0-9]{1,2}-[0-9]{1,2}-[0-9]{4})\ (?P<time>[0-9]{4})'
+        regex = re.compile(pattern, re.VERBOSE|re.IGNORECASE|re.DOTALL)
+        r = regex.search(line.strip())
+        regex.match(line.strip())
+        parts = regex.findall(line)
+        #print parts
+        if parts == []:
+            print line
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(usage='', description='Handle the options we handle.',
@@ -22,6 +42,8 @@ if __name__ == '__main__':
                         help='a two-letter abbreviation for the state we want to process, i.e. CO CA AZ etc.')
     parser.add_argument("-v", "--verbose", dest="verbose", default=False, action="store_true")
     args = parser.parse_args()
+    print dir(args)
+    print args.state
 
     if args.verbose:
         doctest.testmod(verbose=args.verbose)
